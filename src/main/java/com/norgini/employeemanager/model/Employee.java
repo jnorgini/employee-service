7885358f246409cb54efa4dba9f91b01f1c96ba1
jnorgini <jnorgini@gmail.com>
2023-07-13 17:54:52 -0300

    postgres

--- src/main/java/com/norgini/employeemanager/model/Employee.java
+++ src/main/java/com/norgini/employeemanager/model/Employee.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +19,6 @@ import lombok.ToString;
 @Setter
 @ToString
 @Entity
-@Table(name = "employee")
 public class Employee implements Serializable {
 	private static final long serialVersionUID = 1L;
 
